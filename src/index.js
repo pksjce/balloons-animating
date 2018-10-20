ebe494d86aa58fc9f08718c543ef7a27b755ce49
pksjce <pksjce@gmail.com>
2018-10-20 23:00:11 +1100

    Add enhancements

--- src/index.js
+++ src/index.js
@@ -2,90 +2,135 @@ import { TweenMax } from "gsap";
 
 const X = 200;
 const Y = 200;
+const speed = () => Math.random() + 4;
+const delay = () => Math.random();
+
+const createBalloon = ({ id, fillColor }) => {
+  return `
+  <g id='${id}'>
+  <defs>
+  <radialGradient id="RadialGradient${id}" cx="0.60" cy="0.38" r="0.35">
+    <stop offset="0%" stop-color="white"/>
+    <stop offset="100%" stop-color="${fillColor}"/>
+  </radialGradient>
+</defs>
+    <path id='balloon' fill='url(#RadialGradient${id})' stroke='${fillColor}' d="M${X -
+    1} ${Y - 4} c-85 -100 85 -100 2 0"/>
+
+    <path id='balloon-bottom' fill='${fillColor}' d="M${X + 3} ${Y -
+    2} h-5 l2 -2 l2 0 " stroke='${fillColor}' />
+    <path id='thread' d="M${X + 7} ${Y -
+    4} h-4 q-20,2,1,45 q15,30,-3,49 q-21,22,4,65" fill='none' stroke='purple'/>
+  </g>
+  `;
+};
+
+const balloons = [
+  {
+    id: "balloon1",
+    fillColor: "magenta"
+  },
+  {
+    id: "balloon2",
+    fillColor: "orange"
+  },
+  {
+    id: "balloon3",
+    fillColor: "papayawhip"
+  },
+  {
+    id: "balloon4",
+    fillColor: "lightcoral"
+  },
+  {
+    id: "balloon5",
+    fillColor: "chocolate"
+  },
+  {
+    id: "balloon6",
+    fillColor: "lightblue"
+  }
+];
+
+const balloonhtml = balloons.map(createBalloon).join("");
 
 document.getElementById("app").innerHTML = `
 
 <svg viewBox="-10 -10 500 500">
-    <g id='balloon1'>
-    <path id='balloon' fill='red' stroke='red' d="M${X - 1} ${Y -
-  4} c-85 -100 85 -100 2 0"/>
-    <path id='balloon-inside' fill='white' stroke='white' d='M${X + 10} ${Y -
-  60} l5 -3 l-2 15 z' />
-    <path id='balloon-bottom' fill='red' d="M${X} ${Y} l-5 0 l5 -5 l5 5 l-8 0" stroke='red' />
-    <path id='thread' d="M${X} ${Y} q10 5 0 10 q-10 5 5 20 q10 10 0 20" fill='none' stroke='purple'/>
-    </g>
-    <g id='balloon2'>
-    <path id='balloon' fill='blue' stroke='blue' d="M${X - 1} ${Y -
-  4} c-85 -100 85 -100 2 0"/>
-    <path id='balloon-inside' fill='white' stroke='white' d='M${X + 10} ${Y -
-  60} l5 -3 l-2 15 z' />
-    <path id='balloon-bottom' fill='blue' d="M${X} ${Y} l-5 0 l5 -5 l5 5 l-8 0" stroke='blue' />
-    <path id='thread' d="M${X} ${Y} q10 5 0 10 q-10 5 5 20 q10 10 0 20" fill='none' stroke='purple'/>
-    </g>
-    <g id='balloon3'>
-    <path id='balloon' fill='purple' stroke='purple' d="M${X - 1} ${Y -
-  4} c-85 -100 85 -100 2 0"/>
-    <path id='balloon-inside' fill='white' stroke='white' d='M${X + 10} ${Y -
-  60} l5 -3 l-2 15 z' />
-    <path id='balloon-bottom' fill='purple' d="M${X} ${Y} l-5 0 l5 -5 l5 5 l-8 0" stroke='purple' />
-    <path id='thread' d="M${X} ${Y} q10 5 0 10 q-10 5 5 20 q10 10 0 20" fill='none' stroke='purple'/>
-    </g>
-    <g id='balloon4'>
-    <path id='balloon' fill='lightgreen' stroke='lightgreen' d="M${X - 1} ${Y -
-  4} c-85 -100 85 -100 2 0"/>
-    <path id='balloon-inside' fill='white' stroke='white' d='M${X + 10} ${Y -
-  60} l5 -3 l-2 15 z' />
-    <path id='balloon-bottom' fill='lightgreen' d="M${X} ${Y} l-5 0 l5 -5 l5 5 l-8 0" stroke='lightgreen' />
-    <path id='thread' d="M${X} ${Y} q10 5 0 10 q-10 5 5 20 q10 10 0 20" fill='none' stroke='purple'/>
-    </g>
+    ${balloonhtml}
 </svg>
 `;
 
 TweenMax.fromTo(
   "#balloon1",
-  1,
+  speed(),
   {
     css: { transform: "translate(-100px, 400px)" }
   },
   {
-    css: { transform: "translate(0px, -280px)" },
-    repeat: -1
+    css: { transform: "translate(0px, -400px)" },
+    repeat: -1,
+    delay: delay()
   }
 );
 
 TweenMax.fromTo(
   "#balloon2",
-  1,
+  speed(),
   {
     css: { transform: "translate(-20px, 400px)" }
   },
   {
-    css: { transform: "translate(-50px, -280px)" },
+    css: { transform: "translate(-50px, -400px)" },
     repeat: -1,
-    delay: 0.3
+    delay: delay()
   }
 );
 TweenMax.fromTo(
   "#balloon3",
-  1,
+  speed(),
   {
     css: { transform: "translate(50px, 400px)" }
   },
   {
     css: { transform: "translate(-180px, -280px)" },
     repeat: -1,
-    delay: 0.4
+    delay: delay()
   }
 );
 TweenMax.fromTo(
   "#balloon4",
-  1,
+  speed(),
   {
     css: { transform: "translate(-40px, 400px)" }
   },
   {
     css: { transform: "translate(200px, -280px)" },
     repeat: -1,
-    delay: 0.1
+    delay: delay()
+  }
+);
+TweenMax.fromTo(
+  "#balloon5",
+  speed(),
+  {
+    css: { transform: "translate(-40px, 400px)" }
+  },
+  {
+    css: { transform: "translate(20px, -280px)" },
+    repeat: -1,
+    delay: delay()
+  }
+);
+TweenMax.fromTo(
+  "#balloon6",
+  speed(),
+  {
+    css: { transform: "translate(-40px, 400px)" }
+  },
+  {
+    css: { transform: "translate(400px, -200px)" },
+    repeat: -1,
+    delay: delay()
   }
 );
